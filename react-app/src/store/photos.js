d789f1005af9c0fbdc5046bc150d207d6f3deb49
Jon <jon.ezana1@gmail.com>
2023-08-26 15:03:07 -0400

    working on photos and session reducer

--- react-app/src/store/photos.js
+++ react-app/src/store/photos.js
@@ -5,4 +5,25 @@ const getAllPhotos = (photos) => ({
     payload: photos
 });
 
-export const thunkGetAllPhotos = () =>
+export const thunkGetAllPhotos = () => async (dispatch) => {
+    const res = await fetch('/api/photos/all');
+    if (res.ok) {
+        const photos = await res.json();
+        dispatch(getAllPhotos(photos));
+        return photos;
+    }
+}
+
+const initialState = {allPhotos: {}, singlePhoto: {}, currentUserPhotos: {}};
+
+export default function photosReducer(state = initialState, action) {
+    switch(action.type) {
+        case GET_ALL_PHOTOS: {
+            const newState = {...state};
+            action.payload.forEach(photo => {
+                newState.allPhotos[photo.id] = photo;
+            });
+            return newState;
+        }
+    }
+}
