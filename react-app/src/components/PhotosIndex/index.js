7b624d223586c41b68944d3cb0118e63ee50c005
Ludia <lypark1985@gmail.com>
2023-08-26 18:15:34 -0500

    stuck on user info

--- react-app/src/components/PhotosIndex/index.js
+++ react-app/src/components/PhotosIndex/index.js
@@ -2,33 +2,38 @@ import {useEffect} from "react";
 import { thunkGetAllPhotos } from "../../store/photos";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import './PhotosIndex.css';
 
 export const PhotosIndex = () => {
     const dispatch = useDispatch();
     const history = useHistory();
     const fotos = useSelector(state => {
-        console.log('IN THE USESELECTOR....', state)
         return state.photos.allPhotos
-    });
-    console.log('FOTOS....', fotos)
+    }); 
+    
     useEffect(() => {
         dispatch(thunkGetAllPhotos());
     }, [dispatch])
-
-    // const photos = [];
-    // Object.values(fotos).forEach(photo => photos.push(photo));
+    
     const photos = [...Object.values(fotos)];
-    console.log('PHOTOS....', photos)
 
-    // const sortedPhotos = photos.sort((a, b) => )
+    // key attribute?
 
     return (
-        <>
-            {photos.map(photo =>
-                <div className='photos-container' title={`${photo.name}`} onClick={() => history.push(`/photos/${photo.id}`)} key={photo.id} alt={photo.title}>
-                    <img className="individual-photo-container" src={photo.url}></img>
-                </div>
-            )}
-        </>
+        <div className='container-container'>
+            <div className='all-photos-container'>
+                {photos.map(photo =>
+                    // {if photo.user_id === }
+                    <span className='all-photos-card' title={photo.name} onClick={() => history.push(`/photos/${photo.id}`)} key={photo.id}>
+                        <img className='all-photos-pic' src={photo.url} alt={photo.title}></img>
+                        <div className='bottom-half'>
+                            <span>
+
+                            </span>
+                        </div>
+                    </span>
+                )}
+            </div>
+        </div>
     )
 }
