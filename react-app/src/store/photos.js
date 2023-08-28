4c3fdc4efb90c0c7d235a699dd2370d66381eecb
Jon <jon.ezana1@gmail.com>
2023-08-28 18:55:30 -0400

    updated store for Create Photo and Update Photo

--- react-app/src/store/photos.js
+++ react-app/src/store/photos.js
@@ -1,5 +1,9 @@
 const GET_ALL_PHOTOS = "photos/GET_ALL";
 const GET_SINGLE_PHOTO = "photos/getSinglePhoto";
+const GET_CURRENT_USER_PHOTOS = "photos/getCurrentUserPhotos";
+const DELETE_PHOTO = "photos/DELETE_PHOTO";
+const CREATE_PHOTO = "photos/createPhoto";
+const UPDATE_PHOTO = "photos/updatePhoto";
 
 const getAllPhotos = (photos) => ({
     type: GET_ALL_PHOTOS,
@@ -10,6 +14,26 @@ const getSinglePhoto = (photo) => ({
     type: GET_SINGLE_PHOTO,
     payload: photo
 });
+
+const getCurrentUserPhotos = (photos) => ({
+    type: GET_CURRENT_USER_PHOTOS,
+    payload: photos
+})
+
+const deletePhoto = (photoId) => ({
+    type: DELETE_PHOTO,
+    payload: photoId
+});
+
+const createPhoto = (photo) => ({
+    type: CREATE_PHOTO,
+    payload: photo
+});
+
+const updatePhoto = (photo) => ({
+    type: UPDATE_PHOTO,
+    payload: photo
+})
 
 export const thunkGetAllPhotos = () => async (dispatch) => {
     const res = await fetch('/api/photos/all');
@@ -31,6 +55,65 @@ export const thunkGetSinglePhoto = (id) => async (dispatch) => {
     }
 };
 
+export const thunkGetCurrentUserPhotos = (UserId) => async (dispatch) => {
+    const res = await fetch('/api/photos/all');
+    if (res.ok) {
+        const photoData = await res.json();
+        const filteredPhotos = photoData.photos.filter(photo => photo.userId == UserId);
+        dispatch(getCurrentUserPhotos(filteredPhotos));
+        return filteredPhotos;
+    }
+}
+
+export const thunkDeletePhoto = (photoId) => async (dispatch) => {
+    const res = await fetch(`/api/photos/delete/${photoId}`, {
+        method: 'DELETE'
+    })
+    if (res.ok) {
+        await dispatch(deletePhoto(photoId))
+    }
+}
+
+export const thunkCreatePhoto = (photoData) => async (dispatch) => {
+    const {title, url, description, previewImg, userId} = photoData;
+    const res = await fetch('/api/photos/new', {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            title: title,
+            url: url,
+            description: description,
+            preview_img: previewImg,
+            user_id: userId
+        })
+    });
+    if (res.ok) {
+        const photo = await res.json();
+        dispatch(createPhoto(photo));
+        return photo;
+    } else {
+        const err = await res.json();
+        return err['errors'];
+    }
+}
+
+export const thunkUpdatePhoto = (photoId, photoData) => async (dispatch) => {
+    const res = await fetch(`/api/photos/edit/${photoId}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(photoData)
+    });
+    if (res.ok) {
+        const updatedPhoto = await res.json();
+        dispatch(updatePhoto(updatedPhoto));
+        return updatedPhoto;
+    } else {
+        const err = await res.json();
+        return err['errors'];
+    }
+
+}
+
 const initialState = {allPhotos: {}, singlePhoto: {}, currentUserPhotos: {}};
 
 export default function photosReducer(state = initialState, action) {
@@ -44,6 +127,36 @@ export default function photosReducer(state = initialState, action) {
         }
         case GET_SINGLE_PHOTO: {
             return {...state, allPhotos: {...state.allPhotos}, singlePhoto: {...action.payload}, currentUserPhotos: {}}
+        }
+        case GET_CURRENT_USER_PHOTOS: {
+            const newState = {...state, allPhotos: {...state.allPhotos}, currentUserPhotos: {...state.currentUserPhotos}};
+            action.payload.forEach(photo => {
+                newState.currentUserPhotos[photo.id] = photo;
+            });
+            return newState;
+        }
+        case DELETE_PHOTO: {
+            const newState = {...state, allPhotos: {...state.allPhotos}, singlePhoto: {...state.singlePhoto}, currentUserPhotos: {...state.currentUserPhotos}}
+            delete newState.allPhotos[action.payload]
+            delete newState.currentUserPhotos[action.payload]
+            delete newState.singlePhoto;
+            return {...newState, allPhotos: {...newState.allPhotos}, currentUserPhotos: {...newState.currentUserPhotos}, singlePhoto: {...newState.singlePhoto}};
+        }
+        case CREATE_PHOTO: {
+            return {
+                ...state,
+                allPhotos: {...state.allPhotos, [action.payload.id]: action.payload},
+                currentUserPhotos: {...state.currentUserPhotos, [action.payload.id]: action.payload},
+                singlePhoto: action.payload
+            }
+        }
+        case UPDATE_PHOTO: {
+            return {
+                ...state,
+                allPhotos: {...state.allPhotos, [action.payload.id]: action.payload},
+                currentUserPhotos: {...state.currentUserPhotos, [action.payload.id]: action.payload},
+                singlePhoto: action.payload
+            }
         }
         default:
             return state;
