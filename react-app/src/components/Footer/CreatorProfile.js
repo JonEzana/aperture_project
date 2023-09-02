8a42f8393fef7f7f06fe9878cc32f47adfb5f4d8
Ludia <lypark1985@gmail.com>
2023-09-02 00:19:47 -0500

    finished footer

--- react-app/src/components/Footer/CreatorProfile.js
+++ react-app/src/components/Footer/CreatorProfile.js
@@ -1,8 +1,11 @@
-export const CreatorProfile = ({creator}) => {
+import { NavLink } from "react-router-dom";
+
+export const CreatorProfile = ({creator, creators}) => {
     return  (
-        <div className="creator-card">
-            <em>{creator.name}</em>
-            <a href={creator.github} className="github-links"><i className="fab fa-github"></i></a>
-        </div>
+        <NavLink to={creator.github} id='creator-item' key={creators.indexOf(creator)}>
+            <p id='creator-name'>{creator.name}</p>
+            <i id="github-icon" className="fab fa-github"></i>
+        </NavLink>
+
     )
 }
