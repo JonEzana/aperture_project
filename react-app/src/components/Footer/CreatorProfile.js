0b259bf7e32a78cb9e5d3c79395f5572375b5076
Jon <jon.ezana1@gmail.com>
2023-09-05 09:50:04 -0400

    change footer navlinks into a tags

--- react-app/src/components/Footer/CreatorProfile.js
+++ react-app/src/components/Footer/CreatorProfile.js
@@ -1,11 +1,9 @@
-import { NavLink } from "react-router-dom";
-
 export const CreatorProfile = ({creator, creators}) => {
     return  (
-        <NavLink to={creator.github} id='creator-item' key={creators.indexOf(creator)}>
+        <a href={creator.github} id='creator-item' key={creators.indexOf(creator)}>
             <p id='creator-name'>{creator.name}</p>
             <i id="github-icon" className="fab fa-github"></i>
-        </NavLink>
+        </a>
 
     )
 }
